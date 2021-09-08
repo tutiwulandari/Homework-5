c718d4641f3ab14900145be0d26cd6aea1046a08
putri sanjiwani dewi <putrisanjiwanidewi@gmail.com>
2021-09-08 17:04:52 +0900

    add page logout

---
+++ src/pages/Status/Logout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavbarComponent from "../../assets/components/navbar/NavbarComponent"
+import '../Status/logout.css'
+
+
+function Logout() {
+
+  return (
+    <div>
+      <NavbarComponent />
+      <div className='logout'>
+      <h1>Keluar</h1>
+    </div>
+    </div>
+  );
+}
+
+export default Logout;
