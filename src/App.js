ae56290c6daca97d0a1f1a41f8be9807d6e5b91e
putri sanjiwani dewi <putrisanjiwanidewi@gmail.com>
2021-09-08 17:07:20 +0900

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Login from "./pages/login/Login"
-
+import logo from './logo.svg';
+import './App.css';
+import TransaksiPage from './pages/Transaksi/TransaksiPage';
+import Login from "./pages/login/Login";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Logout from "./pages/Status/Logout";
+import AuthorizedRoute from "./AuthorizedRoute";
+import RestrictedWrapper from "./RestrictedWrapper";
+import { AuthorizedContextProvider } from "./AuthorizedContext";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <AuthorizedContextProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <RestrictedWrapper>
+              <Login />
+            </RestrictedWrapper>
+          </Route>
+          <AuthorizedRoute path="/Transaksi" exact component={TransaksiPage}></AuthorizedRoute>
+          <Route path="/Signout" exact component={Logout} />
+          <AuthorizedRoute path="/Home" exact component={Home}></AuthorizedRoute>
+        </Switch>
+      </Router>
+    </AuthorizedContextProvider>
   );
 }
 
