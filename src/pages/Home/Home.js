bb73df946484642f182b01ec031ba2dd8ae5c005
putri sanjiwani dewi <putrisanjiwanidewi@gmail.com>
2021-09-12 20:15:25 +0900

    pengunaan Text dan Title ant design

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,99 +1,86 @@
-import React, {Component} from 'react';
-// import { Form, Col, Row, Button, Navbar} from 'react-bootstrap';
-import {
-  Row,
-  Col,
-  Form,
-  Input,
-  Button,
-  Card
+import React, { Component } from "react";
+import { Row, Col, Form, Input, Button, Card, Typography } from "antd";
+import "./Home.css";
+import NavbarComponent from "../../assets/components/navbar/NavbarComponent";
+import { useAuthorizedContext } from "../../AuthorizedContext";
 
-} from "antd";
-import'./Home.css'
-import NavbarComponent from "../../assets/components/navbar/NavbarComponent"
-import {useAuthorizedContext} from '../../AuthorizedContext'
+const { Title, Text } = Typography;
 
 function Home() {
-  const { isLoggedIn, userLevel } = useAuthorizedContext()
-  console.log("value >> ", isLoggedIn, userLevel)
-  return (  
+  const { isLoggedIn, userLevel } = useAuthorizedContext();
+  console.log("value >> ", isLoggedIn, userLevel);
+  return (
     <div className="outer-home">
       <NavbarComponent />
-
       <div className="statusTransaksi">
         <div className="title">
-          <p>Transaksi Saat Ini:</p>
+          <Title level={3}>Transaksi Saat Ini:</Title>
         </div>
         <div className="resume">
-          <Card title=" "  style={{ width: "30%"}}>
-          <Form style={{ marginLeft: "10%"}}>
-            <Row >
-              <Col style={{width: "35%"}}>
-              <p>Waktu Request </p>
-              </Col>
-            <Col offset={0} style={{width: "65%"}}> 
-            <p> : {new Date().toLocaleString()}</p>
-            </Col>
-            </Row>
-            <Row >
-            <Col style={{width: "35%"}}>
-              <p>Jenis Transaksi</p>
-              </Col>
-              <Col style={{width: "65%"}}> 
-            <p>: Setoran Pinjaman </p>
-            </Col>
-            
-            </Row>
+          <Card title=" " style={{ width: "30%" }}>
+            <Form style={{ marginLeft: "10%" }}>
+              <Row style={{ marginBottom: "8px" }}>
+                <Col style={{ width: "35%" }}>
+                  <Text>Waktu Request </Text>
+                </Col>
+                <Col offset={0} style={{ width: "65%" }}>
+                  <Text> : {new Date().toLocaleString()}</Text>
+                </Col>
+              </Row>
+              <Row style={{ marginBottom: "8px" }}>
+                <Col style={{ width: "35%" }}>
+                  <Text>Jenis Transaksi</Text>
+                </Col>
+                <Col style={{ width: "65%" }}>
+                  <Text>: Setoran Pinjaman </Text>
+                </Col>
+              </Row>
 
-            <Row >
-            <Col style={{width: "35%"}}>
-              <p>Nominal Transaksi</p>
-              </Col>
-              <Col sstyle={{width: "65%"}}> 
-            <p> : Rp. 10.000.000 </p>
-            </Col>
-            </Row>
+              <Row style={{ marginBottom: "8px" }}> 
+                <Col style={{ width: "35%" }}>
+                  <Text>Nominal Transaksi</Text>
+                </Col>
+                <Col sstyle={{ width: "65%" }}>
+                  <Text> : Rp. 10.000.000 </Text>
+                </Col>
+              </Row>
 
-            <Row >
-            <Col style={{width: "35%"}}>
-              <p>Alamat Agen</p>
-              </Col>
-              <Col style={{width: "65%"}}> 
-            <p> : Jl. Banjaran no 181 </p>
-            </Col>
-            </Row>
+              <Row style={{ marginBottom: "8px" }}>
+                <Col style={{ width: "35%" }}>
+                  <Text>Alamat Agen</Text>
+                </Col>
+                <Col style={{ width: "65%" }}>
+                  <Text> : Jl. Banjaran no 181 </Text>
+                </Col>
+              </Row>
 
-            <Row >
-            <Col style={{width: "35%"}}>
-                <p>Status</p>            
-              </Col>
-              <Col style={{width: "65%"}}>
-                <p>: Menunggu Konfirmasi Agen</p>            
-              </Col> 
-            </Row>
+              <Row style={{ marginBottom: "8px" }}>
+                <Col style={{ width: "35%" }}>
+                  <Text>Status</Text>
+                </Col>
+                <Col style={{ width: "65%" }}>
+                  <Text>: Menunggu Konfirmasi Agen</Text>
+                </Col>
+              </Row>
 
-            <div className="float-right">
-              <Button type="primary" style={{margin:"0px", paddingRight: "15px", 
-              backgroundColor:"#F03D3E", fontWeight:"bold"}}>
+              <div className="float-right">
+                <Button
+                  type="primary"
+                  style={{
+                    margin: "0px",
+                    paddingRight: "15px",
+                    backgroundColor: "#F03D3E",
+                    fontWeight: "bold",
+                  }}
+                >
                   Batalkan
-              </Button>
-            </div>
-          </Form> 
-        
-
-      </Card>
-
-
-
+                </Button>
+              </div>
+            </Form>
+          </Card>
         </div>
-        
       </div>
-
-      
-      
     </div>
-
-    
   );
 }
 
