c8885abdf9d91b2b096f98d324078c36e39060db
grarizki <28901232+grarizki@users.noreply.github.com>
2021-09-30 10:57:54 +0700

    apply prettier and fix Home route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
-import "./App.css";
-import TransaksiPage from "./pages/transaksi/TransaksiPage";
-import Login from "./pages/login/Login";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Logout from "./pages/Status/Logout";
-import AuthorizedRoute from "./AuthorizedRoute";
-import RestrictedWrapper from "./RestrictedWrapper";
-import { AuthorizedContextProvider } from "./AuthorizedContext";
+import { QueryClient, QueryClientProvider } from "react-query"
+import "./App.css"
+import TransaksiPage from "./pages/Transaksi/TransaksiPage"
+import Login from "./pages/login/Login"
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Logout from "./pages/Status/Logout"
+import AuthorizedRoute from "./AuthorizedRoute"
+import RestrictedWrapper from "./RestrictedWrapper"
+import { AuthorizedContextProvider } from "./AuthorizedContext"
 
 // Create a client
 const queryClient = new QueryClient({
@@ -20,7 +17,7 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-});
+})
 
 function App() {
   return (
@@ -39,16 +36,12 @@ function App() {
               component={TransaksiPage}
             ></AuthorizedRoute>
             <Route path="/Signout" exact component={Logout} />
-            <AuthorizedRoute
-              path="/Home"
-              exact
-              component={Home}
-            ></AuthorizedRoute>
+            <AuthorizedRoute path="/Home" exact component={Home}></AuthorizedRoute>
           </Switch>
         </Router>
       </AuthorizedContextProvider>
-     </QueryClientProvider>
-  );
+    </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
