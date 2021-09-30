28fa54e2cf450617f88ec4d288389660b0af1df6
raihannissa123 <raihannissah@gmail.com>
2021-09-30 19:28:17 +0700

    Add testing Home.test

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -19,4 +19,87 @@ describe("Home", () => {
     })
   })
   
+  test("There is a Request", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Request")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Jenis Transaksi", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Jenis transaksi")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Nominal Transaksi", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Nominal Transaksi")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Alamat", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Alamat")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Agen", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Agen")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Status", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Home />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Status")).toBeInTheDocument()
+    })
+  })
+
+  test("There is a Batalkan button", async () => {
+    const queryClient = new QueryClient()
+    return (
+      <QueryClientProvider client={queryClient}>
+        <TransaksiPage />
+      </QueryClientProvider>
+    )
+    await waitFor(() => {
+      expect(screen.getByText("Batalkan")).toBeInTheDocument()
+    })
+  })
 })
